9a6ea0cf5845db8802a7e81b6c10026f208da260
admin <287257255@qq.com>
2017-06-17 12:38:56 +0800

    测试2的修改

--- simple/src/main/java/com/ybao/pullrefreshview/simple/activities/MainActivity.java
+++ simple/src/main/java/com/ybao/pullrefreshview/simple/activities/MainActivity.java
@@ -2,8 +2,8 @@ package com.ybao.pullrefreshview.simple.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.NestedScrollingParent;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.ybao.pullrefreshview.simple.R;
@@ -18,9 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("测试2的修改");
         findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.v("-------", "测试2");
                 startActivity(new Intent(MainActivity.this, FlingActivity.class));
             }
         });
