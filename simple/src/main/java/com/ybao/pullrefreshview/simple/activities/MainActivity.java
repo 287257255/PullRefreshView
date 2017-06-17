32f616dd4e4f5956e6f793d03ecd770638f8a961
admin <287257255@qq.com>
2017-06-17 13:23:12 +0800

    master

--- simple/src/main/java/com/ybao/pullrefreshview/simple/activities/MainActivity.java
+++ simple/src/main/java/com/ybao/pullrefreshview/simple/activities/MainActivity.java
@@ -18,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("测试3的修改");
+        System.out.println("3");
 
         findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v("-------", "测试3");
+                Log.v("-------", "3");
                 startActivity(new Intent(MainActivity.this, FlingActivity.class));
             }
         });
